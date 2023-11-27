1bc539391ec8790798ef1949e31ef1f3af657b4a
ShafqatShahriar <shafqatarefin@gmail.com>
2023-11-27 23:27:37 +0600

    if-else py

---
+++ if_else.py
@@ -0,0 +1,10 @@
+#python if else
+a = 205
+b = 200
+if b > a:
+  print("b is greater than a\n")
+  print(b)
+elif a == b:
+  print(f"a and b are equal\n {b} {a}")
+else:
+  print(f"a is greater than b\n {a}")
